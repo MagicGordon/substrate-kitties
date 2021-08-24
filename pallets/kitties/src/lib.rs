af70c629f23d01634eac3b1d47c159ddd20eb73a
zgdzgdzgdzgd <zgdzgdzgdzgd@163.com>
2021-08-24 18:19:30 +0800

    complete

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -2,6 +2,11 @@
 
 pub use pallet::*;
 
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -13,39 +18,26 @@ pub mod pallet {
     use codec::{Encode, Decode};
     use sp_io::hashing::blake2_128;
 
-    // use parity_scale_codec::{Encode, Decode};
-    use sp_std::ops::Add;
-
-    // use sp_runtime::print;
-    // use sp_runtime::traits::Zero;
-
-    use sp_std::fmt::Debug;
     use sp_runtime::{
         traits::{
-            MaybeDisplay, AtLeast32BitUnsigned, Bounded, MaybeMallocSizeOf
+            AtLeast32BitUnsigned, Bounded
         }
     };
 
     type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
-    #[derive(Encode, Decode)]
+    #[derive(Encode, Decode, Debug)]
     pub struct Kitty(pub [u8; 16]);
 
     // type KittyIndex = u32;
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
-        // type KittyIndex: Add<u32> + codec::EncodeLike + codec::WrapperTypeDecode;
+        type KittyIndex: Parameter + AtLeast32BitUnsigned + Default + Copy + Bounded;
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
         type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
         type ValueBase: Get<BalanceOf<Self>>;
         type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
-        // Add<u32>  + Parameter + Member + Eq + PartialEq + Ord + PartialOrd + Default + Copy + codec::EncodeLike
-        type KittyIndex: 
-        // Parameter + Member + Eq + PartialEq + Ord + PartialOrd + Default + Copy;
-        Parameter + Member + MaybeSerializeDeserialize + Debug + MaybeDisplay +
-            AtLeast32BitUnsigned + Default + Bounded + Copy + sp_std::hash::Hash +
-            sp_std::str::FromStr + MaybeMallocSizeOf + Ord + PartialOrd + Eq + PartialEq + Add<u32> + codec::EncodeLike; 
     }
 
     #[pallet::pallet]
@@ -53,16 +45,18 @@ pub mod pallet {
     pub struct Pallet<T>(_);
 
     #[pallet::event]
-    #[pallet::metadata(T::AccountId = "AccountId")]
+    #[pallet::metadata(T::AccountId = "AccountId", BalanceOf<T> = "Balance")]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         KittiesCreate(T::AccountId, T::KittyIndex),
         KittyTransfer(T::AccountId, T::AccountId, T::KittyIndex),
+        BuyKitty(T::AccountId, T::AccountId, BalanceOf<T>, T::KittyIndex),
+        SellKitty(T::AccountId, T::AccountId, BalanceOf<T>, T::KittyIndex),
     }
 
     #[pallet::storage]
 	#[pallet::getter(fn kitties_count)]
-    pub type KittiesCount<T> = StorageValue<_, u32>;
+    pub type KittiesCount<T: Config> = StorageValue<_, T::KittyIndex>;
     
     #[pallet::storage]
 	#[pallet::getter(fn kitties)]
@@ -78,7 +72,8 @@ pub mod pallet {
         NotOwner,
         SameParentIndex,
         InvalidKittyIndex,
-        CreateKittiesReserveFailed,
+        ReserveFailed,
+        BuyerIsOwner,
     }
 
     #[pallet::call]
@@ -86,30 +81,11 @@ pub mod pallet {
         #[pallet::weight(0)]
         pub fn create(origin: OriginFor<T>) -> DispatchResult {
             let who = ensure_signed(origin)?;
-            // let kitty_id = match Self::kitties_count(){
-            //     Some(id) => {
-            //         ensure!(id != KittyIndex::max_value(), Error::<T>::KittiesCountOverflow);
-            //         id
-            //     },
-            //     None => {
-            //         1
-            //     }
-            // };
-
+            
             let dna = Self::random_value(&who);
 
-            // Kitties::<T>::insert(kitty_id, Some(Kitty(dna)));
-
-            // Owner::<T>::insert(kitty_id, Some(who.clone()));
-
-            // KittiesCount::<T>::put(kitty_id + 1);
-
-            // T::Currency::reserve(&who, T::ValueBase::get()).map_err(|_| Error::<T>::CreateKittiesReserveFailed)?;
-
-            // Self::deposit_event(Event::KittiesCreate(who.clone(), kitty_id));
-
+            T::Currency::reserve(&who, T::ValueBase::get()).map_err(|_| Error::<T>::ReserveFailed)?;
             Self::create_kitty(&who, &dna)?;
-            T::Currency::reserve(&who, T::ValueBase::get()).map_err(|_| Error::<T>::CreateKittiesReserveFailed)?;
 
             Ok(())
         }
@@ -118,7 +94,9 @@ pub mod pallet {
         pub fn transfer(origin: OriginFor<T>, new_owner: T::AccountId, kitty_id: T::KittyIndex) -> DispatchResult {
             let who = ensure_signed(origin)?;
             ensure!(Some(who.clone()) == Owner::<T>::get(kitty_id), Error::<T>::NotOwner);
+
             Owner::<T>::insert(kitty_id, Some(new_owner.clone()));
+
             Self::deposit_event(Event::KittyTransfer(who, new_owner, kitty_id));
             Ok(())
         }
@@ -130,16 +108,6 @@ pub mod pallet {
             let kitty1 = Self::kitties(kitty_id_1).ok_or(Error::<T>::InvalidKittyIndex)?;
             let kitty2 = Self::kitties(kitty_id_2).ok_or(Error::<T>::InvalidKittyIndex)?;
 
-            // let kitty_id = match Self::kitties_count(){
-            //     Some(id) => {
-            //         ensure!(id != KittyIndex::max_value(), Error::<T>::KittiesCountOverflow);
-            //         id
-            //     },
-            //     None => {
-            //         1
-            //     }
-            // };
-
             let dna_1 = kitty1.0;
             let dna_2 = kitty2.0;
 
@@ -150,40 +118,34 @@ pub mod pallet {
                 new_dna[i] = (selector[i] & dna_1[i]) | (!selector[i] & dna_2[i]);
             }
 
-            // Kitties::<T>::insert(kitty_id, Some(Kitty(new_dna)));
-
-            // Owner::<T>::insert(kitty_id, Some(who.clone()));
-
-            // KittiesCount::<T>::put(kitty_id + 1);
-
-            // Self::deposit_event(Event::KittiesCreate(who, kitty_id));
-
             Self::create_kitty(&who, &new_dna)?;
 
             Ok(())
         }
 
         #[pallet::weight(0)]
-        pub fn buy(origin: OriginFor<T>, kitty_id: T::KittyIndex) -> DispatchResult {
+        pub fn buy(origin: OriginFor<T>, kitty_id: T::KittyIndex, value: BalanceOf<T>) -> DispatchResult {
             let who = ensure_signed(origin)?;
             let _kitty = Self::kitties(kitty_id).ok_or(Error::<T>::InvalidKittyIndex)?;
             let to = Owner::<T>::get(kitty_id).ok_or(Error::<T>::InvalidKittyIndex)?;
-            // ensure!(who == to, Error::<T>::ClaimNotExist);
-            Owner::<T>::insert(kitty_id, Some(who.clone()));
-            T::Currency::transfer(&who, &to, T::ValueBase::get(), ExistenceRequirement::AllowDeath)?;
-            T::Currency::unreserve(&to, T::ValueBase::get());
+            ensure!(who != to, Error::<T>::BuyerIsOwner);
+
+            Self::change_currency_owner(&to, &who, kitty_id, value)?;
+
+            Self::deposit_event(Event::BuyKitty(to, who, value, kitty_id));
             Ok(())
         }
 
         #[pallet::weight(0)]
-        pub fn sell(origin: OriginFor<T>, kitty_id: T::KittyIndex, buyer: T::AccountI) -> DispatchResult {
+        pub fn sell(origin: OriginFor<T>, kitty_id: T::KittyIndex, buyer: T::AccountId, value: BalanceOf<T>) -> DispatchResult {
             let who = ensure_signed(origin)?;
-            // let _ = Self::kitties(kitty_id).ok_or(Error::<T>::InvalidKittyIndex)?;
-            // let to = Owner::<T>::get(kitty_id).ok_or(Error::<T>::InvalidKittyIndex)?;
-            // // ensure!(who == to, Error::<T>::ClaimNotExist);
-            // Owner::<T>::remove(kitty_id);
-            // T::Currency::transfer(&who, &to, T::ValueBase::get(), ExistenceRequirement::AllowDeath)?;
-            // T::Currency::unreserve(&to, T::ValueBase::get());
+            let _ = Self::kitties(kitty_id).ok_or(Error::<T>::InvalidKittyIndex)?;
+            let to = Owner::<T>::get(kitty_id).ok_or(Error::<T>::InvalidKittyIndex)?;
+            ensure!(who == to, Error::<T>::NotOwner);
+
+            Self::change_currency_owner(&who, &buyer, kitty_id, value)?;
+
+            Self::deposit_event(Event::SellKitty(who, buyer, value, kitty_id));
             Ok(())
         }
     }
@@ -207,7 +169,7 @@ pub mod pallet {
                     id
                 },
                 None => {
-                    1
+                    1u32.into()
                 }
             };
 
@@ -215,12 +177,18 @@ pub mod pallet {
 
             Owner::<T>::insert(kitty_id, Some(who.clone()));
 
-            KittiesCount::<T>::put(kitty_id + 1);
+            KittiesCount::<T>::put(kitty_id + 1u32.into());
 
             Self::deposit_event(Event::KittiesCreate(who.clone(), kitty_id));
 
             Ok(())
         }
 
+        fn change_currency_owner(from: &T::AccountId, to: &T::AccountId, kitty_id: T::KittyIndex, value: BalanceOf<T>) -> Result<(), DispatchError> {
+            Owner::<T>::insert(kitty_id, Some(to.clone()));
+            T::Currency::transfer(&to, &from, value, ExistenceRequirement::AllowDeath)?;
+            T::Currency::unreserve(&from, T::ValueBase::get());
+            Ok(())
+        }
     }
 }
---
+++ pallets/kitties/src/tests.rs
@@ -0,0 +1,216 @@
+use crate::{Error, Kitties, KittiesCount, Owner, mock::*, self as pallet_balances};
+use frame_support::{assert_ok, assert_noop};
+
+fn events() -> Vec<Event> {
+	let evt = System::events().into_iter().map(|evt| evt.event).collect::<Vec<_>>();
+
+	System::reset_events();
+
+	evt
+}
+
+fn last_event() -> Event {
+	System::events().pop().expect("Event expected").event
+}
+
+#[test]
+fn create_works() {
+	new_test_ext().execute_with(|| {
+		assert_eq!(
+			KittiesModule::kitties_count(),
+			None
+		);
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_eq!(
+			KittiesModule::kitties_count(),
+			Some(2)
+		);
+		assert_eq!(
+			<Test as super::Config>::Currency::reserved_balance(1),
+			100000000000000
+		);
+		// assert_eq!(
+		// 	events(),
+		// 	[
+		// 		// <Test as super::Config>::Event::System(Balance::Event::Reserved(1, 100000000000000)),
+		// 		<Test as super::Config>::Event::KittiesModule(crate::Event::KittiesCreate(1, 1)),
+		// 	]
+		// );
+		assert_eq!(
+			last_event(),
+			<Test as super::Config>::Event::KittiesModule(crate::Event::KittiesCreate(1, 1))
+		);
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_eq!(
+			KittiesModule::kitties_count(),
+			Some(3)
+		);
+		assert_eq!(
+			<Test as super::Config>::Currency::reserved_balance(1),
+			200000000000000
+		);
+
+	});
+}
+
+#[test]
+fn create_reserve_failed() {
+	new_test_ext().execute_with(|| {
+		assert_noop!(
+			KittiesModule::create(Origin::signed(3)),
+			Error::<Test>::ReserveFailed
+		);
+	});
+}
+
+#[test]
+fn transfer_workers(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_ok!(KittiesModule::transfer(Origin::signed(1), 2, 1));
+		assert_eq!(
+			last_event(),
+			<Test as super::Config>::Event::KittiesModule(crate::Event::KittyTransfer(1, 2, 1))
+		);
+	});
+}
+
+
+#[test]
+fn transfer_not_owner(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_noop!(
+			KittiesModule::transfer(Origin::signed(1), 2, 2),
+			Error::<Test>::NotOwner
+		);
+	});
+	
+}
+
+
+
+#[test]
+fn breed_workers(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_ok!(KittiesModule::breed(Origin::signed(1), 1, 2));
+		assert_eq!(
+			KittiesModule::kitties_count(),
+			Some(4)
+		);
+		assert_eq!(
+			last_event(),
+			<Test as super::Config>::Event::KittiesModule(crate::Event::KittiesCreate(1, 3))
+		);
+	});
+}
+
+#[test]
+fn breed_same_parent_index(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_noop!(
+			KittiesModule::breed(Origin::signed(1), 1, 1),
+			Error::<Test>::SameParentIndex
+		);
+	});
+}
+
+#[test]
+fn breed_invalid_kitty_index(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_noop!(
+			KittiesModule::breed(Origin::signed(1), 1, 2),
+			Error::<Test>::InvalidKittyIndex
+		);
+	});
+}
+
+#[test]
+fn buy_workers(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_ok!(KittiesModule::buy(Origin::signed(2), 1, 100 * 1_000 * 1_000_000_000));
+		assert_eq!(
+			<Test as super::Config>::Currency::free_balance(1),
+			10100 * 1_000 * 1_000_000_000
+		);
+		assert_eq!(
+			<Test as super::Config>::Currency::free_balance(2),
+			9900 * 1_000 * 1_000_000_000
+		);
+		assert_eq!(
+			last_event(),
+			<Test as super::Config>::Event::KittiesModule(crate::Event::BuyKitty(1, 2, 100 * 1_000 * 1_000_000_000, 1))
+		);
+	});
+	
+}
+
+#[test]
+fn buy_invalid_kitty_index(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_noop!(
+			KittiesModule::buy(Origin::signed(2), 2, 100 * 1_000 * 1_000_000_000),
+			Error::<Test>::InvalidKittyIndex
+		);
+	});
+}
+
+#[test]
+fn buy_buyer_is_owner(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_noop!(
+			KittiesModule::buy(Origin::signed(1), 1, 100 * 1_000 * 1_000_000_000),
+			Error::<Test>::BuyerIsOwner
+		);
+	});
+}
+
+#[test]
+fn sell_workers(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_ok!(KittiesModule::sell(Origin::signed(1), 1, 2, 100 * 1_000 * 1_000_000_000));
+		assert_eq!(
+			<Test as super::Config>::Currency::free_balance(1),
+			10100 * 1_000 * 1_000_000_000
+		);
+		assert_eq!(
+			<Test as super::Config>::Currency::free_balance(2),
+			9900 * 1_000 * 1_000_000_000
+		);
+		assert_eq!(
+			last_event(),
+			<Test as super::Config>::Event::KittiesModule(crate::Event::SellKitty(1, 2, 100 * 1_000 * 1_000_000_000, 1))
+		);
+	});
+	
+}
+
+#[test]
+fn sell_invalid_kitty_index(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_noop!(
+			KittiesModule::sell(Origin::signed(2), 2, 2, 100 * 1_000 * 1_000_000_000),
+			Error::<Test>::InvalidKittyIndex
+		);
+	});
+}
+
+#[test]
+fn sell_buyer_is_owner(){
+	new_test_ext().execute_with(|| {
+		assert_ok!(KittiesModule::create(Origin::signed(1)));
+		assert_noop!(
+			KittiesModule::sell(Origin::signed(2), 1, 1, 100 * 1_000 * 1_000_000_000),
+			Error::<Test>::NotOwner
+		);
+	});
+}
